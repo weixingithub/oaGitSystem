5a6adeae22301dca308c6cbbc21b706030f3ada1
weixin <763328075@qq.com>
2017-03-14 18:15:49 +0800

    添加年龄字段

--- src/main/java/com/git/test/GitProject.java
+++ src/main/java/com/git/test/GitProject.java
@@ -3,6 +3,13 @@ package com.git.test;
 public class GitProject {
     String gitName;
     String gitNum;
+    Integer userAge;
+	public Integer getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(Integer userAge) {
+		this.userAge = userAge;
+	}
 	public String getGitNum() {
 		return gitNum;
 	}
