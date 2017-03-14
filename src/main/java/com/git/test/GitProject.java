f608fac34d2d807df6dbfe60e8ee0709367e4477
weixin <763328075@qq.com>
2017-03-14 16:09:02 +0800

    maven git helloworld

---
+++ src/main/java/com/git/test/GitProject.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+public class GitProject {
+    String gitName;
+    String gitNum;
+	public String getGitNum() {
+		return gitNum;
+	}
+	public void setGitNum(String gitNum) {
+		this.gitNum = gitNum;
+	}
+}
